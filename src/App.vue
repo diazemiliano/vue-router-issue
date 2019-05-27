5fe5356c5260d6feffc617a10c0ef898bc7751f4
Emiliano Díaz <diazemiliano.personal@gmail.com>
2019-05-27 14:21:49 -0300

    Issue importing two instances of the vue router

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+		<button @click="show = !show">Open</button>
+    <router-link to="/" v-if="show">
+      <input type="password">
+    </router-link>
+	</div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        show: false
+      }
+    }
+  }
+</script>
